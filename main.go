84f158cc80ed50dc01d5c00c0d4a419a5f7876f1
bodevone <aliby.bbb@gmail.com>
2020-12-17 02:13:01 +0600

    Change package name

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 
-	solver "ant-farm/internal/solver"
+	solver "antfarm/internal/solver"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
